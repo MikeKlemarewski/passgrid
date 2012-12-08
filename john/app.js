2cac351ca258d95478017d2ba3bf57e0490da701
John Boxall <john@mobify.me>
2012-12-08 12:31:11 -0800

    Basic video cap script

---
+++ john/app.js
@@ -0,0 +1,53 @@
+// http://127.0.0.1:3000/
+// http://openetherpad.org/q99V0ujRlP
+
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var link = document.getElementById('control');
+
+var isWebkit = false;
+
+var getStream = function(win, fail) {
+    var getUserMedia;
+    if (navigator.getUserMedia) {
+        getUserMedia = 'getUserMedia';
+    } else if (navigator.webkitGetUserMedia) {
+        getUserMedia = 'webkitGetUserMedia';
+        isWebkit = true;
+    } else {
+        return fail();
+    };
+
+    navigator[getUserMedia]({video: true}, win, fail);
+};
+
+var win = function(stream) {
+    var source = isWebkit ? webkitURL.createObjectURL(stream) : stream;
+    video.src = source;
+
+    setTimeout(snap, 1000);
+
+};
+
+
+var fail = function() {
+    console.log("BOOM", arguments)
+    alert("BOOM");
+};
+
+var snap = function() {
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0);
+    context.drawImage(video, 0, 0);
+};
+
+
+
+getStream(win, fail);
+
+document.getElementById("snap").onclick = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    snap();
+};
+
